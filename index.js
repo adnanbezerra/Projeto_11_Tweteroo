a598bc5c5bb1abe47f6a1f5ca9d28bf15be9b99d
seu nome <adnanbezerra@gmail.com>
2023-01-09 21:40:44 -0300

    refactor: use linter on old index file

--- index.js
+++ index.js
@@ -1,61 +1,62 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
-const server = express()
-server.use(cors())
-server.use(express.json())
+const server = express();
+server.use(cors());
+server.use(express.json());
 
-const usersList = []
+const usersList = [];
 
-const tweetsList = []
+const tweetsList = [];
 
 server.post('/sign-up', (request, response) => {
-    const user = {
-      username: request.body.username,
-      avatar: request.body.avatar  
-    }
+  const user = {
+    username: request.body.username,
+    avatar: request.body.avatar,
+  };
 
-    usersList.push(user)
+  usersList.push(user);
 
-    response.send(request.body)
-})
+  response.send(request.body);
+});
 
 server.get('/tweets', (request, response) => {
-    response.send(getLastTenTweets())
-})
+  response.send(getLastTenTweets());
+});
 
 function getLastTenTweets() {
-    const lastTenTweets = [];
+  const lastTenTweets = [];
 
-    for(let i = 0; i < 10; i++) {
-        if(i === tweetsList.length) break;
-        lastTenTweets.push(setTweet(i))
-    }
+  for (let i = 0; i < 10; i++) {
+    if (i === tweetsList.length) break;
+    lastTenTweets.push(setTweet(i));
+  }
 
-    return lastTenTweets;
+  return lastTenTweets;
 }
 
 function setTweet(index) {
-    const user = usersList.find(user => user.username === tweetsList[index].username);
+  const user = usersList.find(
+    (user) => user.username === tweetsList[index].username,
+  );
 
-    const tweet = {
-        username: user.username,
-        avatar: user.avatar,
-        tweet: tweetsList[index].tweet
-    };
+  const tweet = {
+    username: user.username,
+    avatar: user.avatar,
+    tweet: tweetsList[index].tweet,
+  };
 
-    return(tweet)
+  return tweet;
 }
 
 server.post('/tweets', (request, response) => {
-        
-    const tweet = {
-        username: request.body.username,
-        tweet: request.body.tweet
-    }
-    
-    tweetsList.unshift(tweet)
-    response.send('oi')
-})
+  const tweet = {
+    username: request.body.username,
+    tweet: request.body.tweet,
+  };
+
+  tweetsList.unshift(tweet);
+  response.send('oi');
+});
 
 server.listen(5000);
